a198589e6cd1a6f3e25394c2cb1b8cb072efd189
Karis004 <2862309436@qq.com>
2025-03-04 14:30:38 +0800

    feat: 更新引导页面样式，添加跳转按钮，优化用户导航体验

--- guidance/guidance.js
+++ guidance/guidance.js
@@ -5,15 +5,24 @@ const totalSteps = steps.length;
 
 nextButtons.forEach((btn, index) => {
     btn.addEventListener('click', () => {
-        if (index >= totalSteps - 1) {
-            // continue
-        } else {
-            steps[index + 1].scrollIntoView({ behavior: 'smooth', block: 'center' });
-        }
+        steps[index + 1].scrollIntoView({ behavior: 'smooth', block: 'center' });
+        // if (index + 1 === totalSteps - 1) {
+            
+        // }
     });
 });
 
 const navigator = document.getElementById('ez-navigator');
 navigator.addEventListener("mouseover", () => {
     steps[1].scrollIntoView({ behavior: 'smooth', block: 'center' });
+}, { once: true });
+
+const jumpBtns = document.querySelectorAll('.jump-btn');
+jumpBtns[0].addEventListener('click', () => {
+    window.open('https://moodle.hku.hk/', '_blank');
 });
+
+jumpBtns[1].addEventListener('click', () => {
+    window.open('https://sis-eportal.hku.hk/psp/ptlprod/EMPLOYEE/EMPL/h/?tab=DEFAULT', '_blank');
+});
+
