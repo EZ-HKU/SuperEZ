3e9b86e50f889c38cdcc86ff01ff4ce24321ec7d
Karis004 <2862309436@qq.com>
2025-05-24 22:49:33 +0800

    feat: 添加exam base按钮，优化用户设置界面，支持动态存储选项

--- scripts/lib/elements.js
+++ scripts/lib/elements.js
@@ -270,3 +270,10 @@ window.elements.Link = function (custom, inner) {
         return linkContainer;
     });
 }
+
+window.elements.Label = function (custom, inner) {
+    return ezReactElements.createElement(custom, inner, () => {
+        let labelContainer = document.createElement("label");
+        return labelContainer;
+    });
+};
